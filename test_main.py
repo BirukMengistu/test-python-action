d4cb8c998d6c1e518bb2cc49d97c7ea300b2d35c
Birukmengistu <sheger.shelf3@gmail.com>
2024-10-16 11:49:06 +0200

    add sub function

--- test_main.py
+++ test_main.py
@@ -1,7 +1,8 @@
 # test_main.py
 
 import unittest
-from test import add
+from test import add 
+from test import subtract
 
 class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
@@ -9,6 +10,13 @@ class TestAddFunction(unittest.TestCase):
 
     def test_add_negative_numbers(self):
         self.assertEqual(add(-1, -1), -2)
+class TestSubFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(subtract(2, 3), -1)
 
+    def test_add_negative_numbers(self):
+        self.assertEqual(subtract(-1, -1), -2)
 if __name__ == "__main__":
     unittest.main()
+
+
