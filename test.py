068013da820d87d81d51d0ee48bc52292dab5b05
Birukmengistu <sheger.shelf3@gmail.com>
2024-10-16 12:01:14 +0200

    check status

--- test.py
+++ test.py
@@ -3,6 +3,11 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 if __name__ == "__main__":
-    result = add(2, 3)
-    print(f"The result is {result}")
+    result1 = add(2, 3)
+    result2 = subtract(2, 3)
+    print(f"The result is add {result1}")
+    print(f"The result is  sub {result2}")
